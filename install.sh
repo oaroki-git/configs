bd0fba08ba64911347bc0406607c44bc127e4ba8
Oaroki <121933094+oaroki-git@users.noreply.github.com>
2024-09-30 14:02:28 +0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env zsh
+
+# homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install -cask alttab
+brew install --cask deeper
+brew install --cask appcleaner
+brew install --cask iina
+
+# requires xcode!
+brew install koekeishiya/formulae/yabai
+brew install koekeishiya/formulae/skhd
